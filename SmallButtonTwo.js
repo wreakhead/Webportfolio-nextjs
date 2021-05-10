83a047c0a89a1e06c60e0a21e68e150d5868dfb9
Imran <mohdimran.khan751@gmail.com>
2021-05-11 02:08:35 +0530

    Create SmallButtonTwo.js

---
+++ SmallButtonTwo.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  btnStyle: {
+    fontWeight: "500",
+    fontSize: "12px",
+    
+    background:'#212121',
+    display: "inline-block",
+    padding: "5px 10px",
+    borderRadius: "50px",
+    transition: "0.5s",
+    color: "#64dd17",
+    border: "2px solid #64dd17",
+    textDecoration: "none",
+    "&:hover": {
+      background: "#64dd17",
+      color: "#212121",
+      textDecoration: "none",
+    },
+  },
+});
+
+const  SmallButtonTwo=(props)=> {
+  const classes = useStyles();
+  
+  
+  
+
+  return (
+    <>
+      <a href={props.link} className={classes.btnStyle}>
+        {props.name}
+      </a>
+    </>
+  );
+}
+
+export default SmallButtonTwo;
