fdf87ab16a45e05ce5de1f6855494547fa9eb5bd
IMRAN KHAN <imran7897476558@gmail.com>
2021-05-11 18:23:51 +0530

    layout fixed

--- components/AboutLayout.js
+++ components/AboutLayout.js
@@ -5,10 +5,10 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     
     paper: {
-      padding: theme.spacing(3),
+      padding: theme.spacing(2),
       textAlign: 'center',
       color: theme.palette.text.secondary,
-      width:"30vw"
+      
     },
   }));
 
@@ -18,7 +18,7 @@ const AboutLayout = (data)=>{
         <>
         <Grid   item xs={12} md={6}>
                 <Paper className={classes.paper} elevation={0}>
-                    <img src={data.image} alt={data.title} style={{minWidth:"40vw"}} height={230} />
+                    <img src={data.image} alt={data.title} style={{minWidth:"30vw"}} height={230} />
                 </Paper>
             </Grid>
            <Grid  item xs={12} md={6}>
